22c09264923db24b5804ea23314b9fb4db6456ce
Thomas Lahmer <e0927474@student.tuwien.ac.at>
2017-04-05 15:55:34 +0200

    added testdata, some changes on models

--- auth.py
+++ auth.py
@@ -1,8 +1,6 @@
 from werkzeug.security import safe_str_cmp
 from models import User 
 
-user = User(1, 'user', 'password')
-
 def authenticate(username, password):
     if username == user.username and safe_str_cmp(user.password.encode('utf-8'), password.encode('utf-8')):
         return user
