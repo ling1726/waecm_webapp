763177ca1a1c7f13463c125b6d17b20ee8cfb8a7
raidsnail <raidsnail@github.com>
2017-05-15 21:20:14 +0200

    fixed healthcheck - returns null when monit demon not running

--- api/healthcheck.py
+++ api/healthcheck.py
@@ -8,21 +8,27 @@ healthcheckAPI = Blueprint('healthcheckAPI', __name__)
 def doHealthCheck():
 
     status_request = subprocess.Popen("monit status", shell=True, stdout=subprocess.PIPE)
-    sreq2 = re.split(r'\n\n', status_request.stdout.read())
 
-    #title_info = sreq2[0]
-    process_info = sreq2[1]
-    system_info = sreq2[2]
+    if (status_request.stdout.read() != ""):
 
-    sreq = re.split(r'\s{2,}', process_info)
-    sreq = sreq[1:]
-    process_dict = {'Process' : dict(zip(sreq[0::2], sreq[1::2]))}
+        status_info = re.split(r'\n\n', status_request.stdout.read())
 
-    sreq = re.split(r'\s{2,}', system_info)
-    sreq = sreq[1:]
-    system_dict = {'System' : dict(zip(sreq[0::2], sreq[1::2]))}
+        process_info = status_info[1]
+        system_info = status_info[2]
 
-    message_dict = dict(system_dict.items() + process_dict.items())
+        sreq = re.split(r'\s{2,}', process_info)
+        sreq = sreq[1:]
+        process_dict = {'Process' : dict(zip(sreq[0::2], sreq[1::2]))}
 
-    return jsonify(message_dict)
+        sreq = re.split(r'\s{2,}', system_info)
+        sreq = sreq[1:]
+        system_dict = {'System' : dict(zip(sreq[0::2], sreq[1::2]))}
+
+        message_dict = dict(system_dict.items() + process_dict.items())
+
+        return jsonify(message_dict)
+    else:
+        return jsonify(None)
+
+
 
