f51e0314ec6bcb2e010a5e402dbb7bfbbf1e6c85
lingfan gao <lingfangao@hotmail.com>
2017-05-16 23:04:00 +0200

    monitoring dashboard should work now

--- monitoring/server.py
+++ monitoring/server.py
@@ -6,34 +6,17 @@ import time
 app = Flask(__name__)
 
 
-testData = {
+default = {
   "Process": {
-    "children": "0", 
-    "cpu percent": "0.0%", 
-    "cpu percent total": "0.0%", 
-    "data collected": "Mon, 15 May 2017 20:40:38", 
-    "effective uid": "0", 
-    "gid": "0", 
-    "memory kilobytes": "37.5 MB", 
-    "memory kilobytes total": "37.5 MB", 
-    "memory percent": "0.4%", 
-    "memory percent total": "0.4%", 
-    "monitoring status": "Monitored", 
-    "parent pid": "1", 
-    "pid": "954", 
-    "port response time": "0.010s to localhost:8080 [HTTP via TCPSSL]", 
-    "status": "Running", 
-    "uid": "0", 
-    "uptime": "53m"
+    "cpu percent total": "N/A", 
+    "data collected": "N/A", 
+    "memory kilobytes total": "N/A", 
+    "port response time": "N/A", 
+    "status": "Connection failed", 
+    "uptime": "N/A"
   }, 
   "System": {
-    "cpu": "2.9%us 1.0%sy 0.2%wa", 
-    "data collected": "Mon, 15 May 2017 20:40:38", 
-    "load average": "[0.05] [0.16] [0.20]", 
-    "memory usage": "3.8 GB [49.3%]", 
-    "monitoring status": "Monitored", 
-    "status": "Running", 
-    "swap usage": "440.9 MB [5.4%]"
+    "cpu": "N/A"
   }
 }
 
@@ -42,19 +25,26 @@ testData = {
 def hello_world():
     print ("request sent to jsonplaceholder")
 
-
-    r = requests.get("https://jsonplaceholder.typicode.com/posts/1")
-    r1 = requests.get("https://jsonplaceholder.typicode.com/posts/7")
-
-    return render_template('index.html', data=r.json(), data2=r1.json())
+    return render_template('index.html')
 
 
 @app.route('/status')
 def getStatus():
-    status = {"appserver1":testData, "appserver2":testData}
+    appserver1 = default
+    appserver2 = default
+    try:
+        appserver1 = requests.get("https://appserver1:8080/api/healthcheck", verify=False).json()
+        appserver2 = requests.get("https://appserver2:8080/api/healthcheck", verify=False).json()
+    except:
+        return jsonify({"appserver1":appserver1, "appserver2":appserver2})    
     
+    status = {"appserver1":appserver1, "appserver2":appserver2} 
     return jsonify(status)
 
 
+
+
+
+
 if __name__ == '__main__':
-   app.run(debug=True)
+   app.run(host='0.0.0.0', port=5000, debug=True)
